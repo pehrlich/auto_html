7b71605e3e2be191a3ba18a6056851aaff6a5c79
Peter Ehrlich <peter.i.ehrlich@gmail.com>
2012-03-16 01:27:57 -0400

    no longer incorrectly persist when using setter, no longer advertently persist when using getter

--- lib/auto_html/auto_html_for.rb
+++ lib/auto_html/auto_html_for.rb
@@ -15,6 +15,7 @@ module AutoHtmlFor
       include AutoHtmlFor::InstanceMethods
       before_save :auto_html_prepare
 
+
       define_method("auto_html_prepare") do
         auto_html_methods = self.methods.select { |m| m=~/^auto_html_prepare_/ }
         auto_html_methods.each do |method|
@@ -26,11 +27,19 @@ module AutoHtmlFor
 
       [raw_attrs].flatten.each do |raw_attr|
         define_method("#{raw_attr}#{suffix}=") do |val|
+          # this would work except write_attribute persists, unlike in rails
           #write_attribute("#{raw_attr}#{suffix}", val)
-          write_attr_in_transaction("#{raw_attr}#{suffix}", val)
+          # note that when description_html is read, it is also written
+          # this happens when first being created, so we skip it there
+          if @properties_before_type_cast
+            write_local_property("#{raw_attr}#{suffix}", val)
+          else
+          end
         end
         define_method("#{raw_attr}#{suffix}") do
-          result = read_attribute("#{raw_attr}#{suffix}") || send("auto_html_prepare_#{raw_attr}")
+          # don't write on read because it breaks dirty attribute tracking and is generally a clusterfuck.
+          #result = read_attribute("#{raw_attr}#{suffix}") || send("auto_html_prepare_#{raw_attr}")
+          result = read_local_property("#{raw_attr}#{suffix}")
           result.respond_to?(:html_safe) ?
             result.html_safe :
               result
